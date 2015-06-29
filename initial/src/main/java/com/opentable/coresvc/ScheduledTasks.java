95b56a60756fc634c892161565f329209991f945
Lu Tahmazyan <ltahmazyan@opentable.com>
2015-06-29 13:20:55 -0700

    added config file

--- initial/src/main/java/com/opentable/coresvc/ScheduledTasks.java
+++ initial/src/main/java/com/opentable/coresvc/ScheduledTasks.java
@@ -1,8 +1,7 @@
-package hello;
+package com.opentable.coresvc;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
